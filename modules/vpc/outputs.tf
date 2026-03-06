6f2e6479c707cbb2ed2583eaa47eb21d277ffbc3
Galin Stefanov <galin.stef@abv.bg>
2026-03-06 19:57:47 +0200

    Add S3 Gateway

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -9,3 +9,8 @@ output "public_subnet_ids" {
 output "private_subnet_ids" {
   value = [for s in aws_subnet.private : s.id]
 }
+
+output "s3_gateway_id" {
+  value       = aws_vpc_endpoint.s3.id
+  description = "ID на S3 VPC Gateway"
+}
