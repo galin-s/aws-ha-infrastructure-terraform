379cf3ed7f28f71543a3401f2878f67e4ac9de79
Galin Stefanov <galin.stef@abv.bg>
2026-03-08 20:42:30 +0200

    Add RDS module

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -1,3 +1,7 @@
 output "asg_name" {
   value = aws_autoscaling_group.web_asg.name
 }
+
+# output "ec2_sg_id" {
+#   value = aws_security_group.ec2_sg.id
+# }
--- modules/security/variables.tf
+++ modules/security/variables.tf
@@ -2,3 +2,7 @@ variable "vpc_id" {
   description = "VPC ID where SGs will be created"
   type        = string
 }
+
+variable "environment" {
+  type = string
+}
